b0b6fcd05a9b573165763901a04a0314f2fa488b
Aydin <156261547+FixRin@users.noreply.github.com>
2025-05-28 14:43:15 +0300

    Final

--- Canvas/Shirt.jsx
+++ Canvas/Shirt.jsx
@@ -7,7 +7,8 @@ import state from '../src/Store';
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('../3D-Model/shirt_baked.glb');
+  const { nodes, materials } = useGLTF('/3D-Model/shirt_baked.glb');
+
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
